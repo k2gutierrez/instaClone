b07d65cbcd00b2bf1488bd8f8f1bdd0d6b630bef
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-03-09 15:44:18 -0600

    added sticky bar

--- components/Header.js
+++ components/Header.js
@@ -6,48 +6,46 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 export default function Header() {
   return (
 
-        <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
+        <div className='shadow-sm border-b sticky top-0 bg-white z-30'>
+            <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
 
-             {/* Left */}
-            <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
+                {/* Left */}
+                <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
                 <Image
                     src="https://cdn.pixabay.com/photo/2016/08/15/01/29/instagram-1594387_1280.png" 
                     layout='fill'
                     alt="logo-name"
                     className='object-contain'
                 />
-            </div>
-            <div className='cursor-pointer h-24 w-10 relative lg:hidden'>
+                </div>
+                <div className='cursor-pointer h-24 w-10 relative lg:hidden'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/800px-Instagram-Icon.png" 
                     layout='fill'
                     alt="logo"
                     className='object-contain'
                 />
-            </div>
+                </div>
 
-            {/* Middle */}
-            
-            <div className='relative mt-1'>
+                {/* Middle */}
+
+                <div className='relative mt-1'>
                 <div className='absolute top-2 left-2'>
                     <MagnifyingGlassIcon className='h-5 text-gray-500' />
                 </div>
                 <input type='text' placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md' name="" id="" />
-            </div>
+                </div>
 
-            {/* Right */}
+                {/* Right */}
 
-            <div className='flex space-x-4 items-center'>
+                <div className='flex space-x-4 items-center'>
                 <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
                 <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
                 <Image src='https://cdn-icons-png.flaticon.com/512/1198/1198343.png' className='h-10 rounded-full cursor-pointer' alt='perfil' width={50} height={50} />
-            </div>
+                </div>
 
+            </div>
         </div>
 
-        
-
-        
-    
   )
 }
